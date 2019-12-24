ecfbf655be38f428c32224dc061e5cca4b2c74de
Daniel Jiang <resttime@users.noreply.github.com>
2019-12-24 17:55:48 -0500

    Use new Vec3 struct and implemented indexing

--- main.rs
+++ main.rs
@@ -1,7 +1,22 @@
-struct vec3 {
+use std::ops::Index;
+
+struct Vec3 {
+    e0: f32,
     e1: f32,
     e2: f32,
-    e3: f32,
+}
+
+impl Index<u32> for Vec3 {
+    type Output = f32;
+    fn index(&self, i: u32) -> &Self::Output {
+        match i {
+            0 => &self.e0,
+            1 => &self.e1,
+            2 => &self.e2,
+            _ => panic!(),
+        }
+    }
+}
 }
 
 fn main() {
@@ -11,12 +26,12 @@ fn main() {
     println!("255");
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r: f32 = i as f32 / nx as f32;
-            let g: f32 = j as f32 / ny as f32;
-            let b: f32 = 0.2;
-            let ir: u32 = (255.99*r) as u32;
-            let ig: u32 = (255.99*g) as u32;
-            let ib: u32 = (255.99*b) as u32;
+            let col = Vec3 {e0: i as f32 / nx as f32,
+                            e1: j as f32 / ny as f32,
+                            e2: 0.2};
+            let ir: u32 = (255.99*col[0]) as u32;
+            let ig: u32 = (255.99*col[1]) as u32;
+            let ib: u32 = (255.99*col[2]) as u32;
             println!("{} {} {}", ir, ig, ib)
         }
     }
