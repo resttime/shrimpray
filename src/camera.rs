d8a9c4fce68d34bcababc36d134de32ea0fc22f6
resttime <resttime@users.noreply.github.com>
2019-12-26 16:42:35 -0500

    Add camera lookat, lookfrom, and vup
    Implements arbritray viewpoints

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,4 @@
-use crate::vec3::{Ray, Vec3};
+use crate::vec3::{cross, Ray, Vec3};
 
 pub struct Camera {
     origin: Vec3,
@@ -19,15 +19,19 @@ impl Default for Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f32, aspect: f32) -> Self {
+    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f32, aspect: f32) -> Self {
         let theta = vfov * std::f32::consts::PI / 180.0;
         let half_height = (theta / 2.0).tan();
         let half_width = aspect * half_height;
+        let origin = lookfrom;
 
-        let ll_corner = Vec3::new(-half_width, -half_height, -1.0);
-        let horizontal = Vec3::new(2.0 * half_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, 2.0 * half_height, 0.0);
-        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let w = (lookfrom - lookat).unit();
+        let u = cross(vup, w);
+        let v = cross(w, u);
+
+        let ll_corner = origin - half_width*u - half_height*v - w;
+        let horizontal = 2.0*half_width*u;
+        let vertical = 2.0*half_height*v;
 
         Self {
             origin: origin,
