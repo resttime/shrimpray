0daa7fc16f57fdccd6cb5bc7fbc03cea025f9590
resttime <resttime@users.noreply.github.com>
2019-12-26 15:53:03 -0500

    Add refract function to Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -135,6 +135,19 @@ pub fn reflect(v: Vec3, n: Vec3) -> Vec3 {
     v - 2.0 * dot(v, n) * n
 }
 
+pub fn refract(v: Vec3, n: Vec3, ni_over_nt: f32) -> Option<Vec3> {
+    let uv = v.unit();
+    let dt = dot(uv, n);
+    let discriminant = 1.0 - ni_over_nt * ni_over_nt * (1.0 - dt * dt);
+
+    if discriminant > 0.0 {
+        let refracted = ni_over_nt * (uv - n * dt) - n * discriminant.sqrt();
+        return Some(refracted);
+    }
+
+    None
+}
+
 #[derive(Copy, Clone, Default)]
 pub struct Ray {
     pub a: Vec3,
