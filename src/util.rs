eac111bfa7779c3a05ac3ab1b18d188545a5b976
resttime <resttime@users.noreply.github.com>
2019-12-26 15:53:21 -0500

    Add schlick function
    Approximates the probability reflection happens on glass

--- src/util.rs
+++ src/util.rs
@@ -15,3 +15,10 @@ pub fn random_in_unit_sphere() -> Vec3 {
         }
     }
 }
+
+pub fn schlick(cosine: f32, ref_idx: f32) -> f32 {
+    let mut r0 = (1.0-ref_idx) / (1.0+ref_idx);
+    r0 = r0 * r0;
+
+    r0 + (1.0-r0) * (1.0-cosine).powi(5)
+}
