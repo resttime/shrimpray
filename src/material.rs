79d39110095c42516c6d14694492013f1f488482
resttime <resttime@users.noreply.github.com>
2019-12-26 15:54:55 -0500

    Implement Dielectric material for glass

--- src/material.rs
+++ src/material.rs
@@ -1,6 +1,6 @@
 use crate::hit::HitRecord;
 use crate::util::*;
-use crate::vec3::{dot, reflect, Ray, Vec3};
+use crate::vec3::{dot, reflect, refract, Ray, Vec3};
 
 pub trait Material {
     fn scatter(&self, ray_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)>;
@@ -55,5 +55,45 @@ impl Material for Metal {
             return Some((scattered, attenuation));
         }
         None
+    }
+}
+
+pub struct Dielectric {
+    ref_idx: f32,
+}
+
+
+impl Dielectric {
+    pub fn new(ri: f32) -> Self {
+        Self { ref_idx: ri }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, ray_in: Ray, hit: &HitRecord) -> Option<(Ray, Vec3)> {
+        let outward_normal: Vec3;
+        let ni_over_nt: f32;
+        let cosine: f32;
+
+        if dot(ray_in.direction(), hit.normal) > 0.0 {
+            outward_normal = -1.0 * hit.normal;
+            ni_over_nt = self.ref_idx;
+            cosine = self.ref_idx * dot(ray_in.direction(), hit.normal) / ray_in.direction().mag();
+        } else {
+            outward_normal = hit.normal;
+            ni_over_nt = 1.0 / self.ref_idx;
+            cosine = -1.0 * dot(ray_in.direction(), hit.normal) / ray_in.direction().mag();
+        }
+
+        let reflected = reflect(ray_in.direction(), hit.normal);
+        let attenuation = Vec3::new(1.0, 1.0, 1.0);
+
+        if let Some(refracted) = refract(ray_in.direction(), outward_normal, ni_over_nt) {
+            let refract_prob = 1.0 - schlick(cosine, self.ref_idx);
+            if rand_float() < refract_prob {
+                return Some((Ray::new(hit.p, refracted), attenuation));
+            }
+        }
+        Some((Ray::new(hit.p, reflected), attenuation))
     }
 }
