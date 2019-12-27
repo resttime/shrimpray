1924d1667313452a916893cdb0a20a0509c2d0f5
resttime <resttime@users.noreply.github.com>
2019-12-26 20:10:11 -0500

    Implement a large random scene

--- src/main.rs
+++ src/main.rs
@@ -36,27 +36,129 @@ fn color(r: Ray, world: &Vec<Box<dyn Hittable>>, depth: u32) -> Vec3 {
     return (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0);
 }
 
+
+fn regular_scene() -> Vec<Box<dyn Hittable>> {
+    let world: Vec<Box<dyn Hittable>> = vec![
+        Box::new(Sphere::new(
+            Vec3::new(0.0, 0.0, -1.0),
+            0.5,
+            Rc::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5))),
+        )),
+        Box::new(Sphere::new(
+            Vec3::new(0.0, -100.5, -1.0),
+            100.0,
+            Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))),
+        )),
+        Box::new(Sphere::new(
+            Vec3::new(1.0, 0.0, -1.0),
+            0.5,
+            Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.3)),
+        )),
+        Box::new(Sphere::new(
+            Vec3::new(-1.0, 0.0, -1.0),
+            0.5,
+            Rc::new(Dielectric::new(1.5)),
+        )),
+        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.45,
+            Rc::new(Dielectric::new(1.5)),
+        )),
+    ];
+    world
+}
+
+fn random_scene() -> Vec<Box<dyn Hittable>> {
+    let mut scene: Vec<Box<dyn Hittable>> = Vec::new();
+    scene.push(Box::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, -1.0),
+        1000.0,
+        Rc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5))),
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand_float();
+            let center = Vec3::new(
+                a as f32 + 0.9 * rand_float(),
+                0.2,
+                b as f32 + 0.9 * rand_float(),
+            );
+            if (center - Vec3::new(4.0, 0.2, 0.0)).mag() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    scene.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Lambertian::new(Vec3::new(
+                            rand_float() * rand_float(),
+                            rand_float() * rand_float(),
+                            rand_float() * rand_float(),
+                        ))),
+                    )));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    scene.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Metal::new(
+                            Vec3::new(
+                                0.5 * (1.0 + rand_float()),
+                                0.5 * (1.0 + rand_float()),
+                                0.5 * (1.0 + rand_float()),
+                            ),
+                            0.5 * rand_float(),
+                        )),
+                    )));
+                } else {
+                    // glass
+                    scene.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Rc::new(Dielectric::new(1.5)),
+                    )));
+                }
+            }
+        }
+    }
+    scene.push(Box::new(Sphere::new(
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Dielectric::new(1.5)),
+    )));
+    scene.push(Box::new(Sphere::new(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1))),
+    )));
+    scene.push(Box::new(Sphere::new(
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0)),
+    )));
+    scene
+}
+
 fn main() {
-    let (nx, ny, ns) = (200, 100, 100);
+    let (nx, ny, ns) = (1200, 800, 100);
     println!("P3");
     println!("{} {}", nx, ny);
     println!("255");
 
-    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(7.5, 1.5, 2.0);
+    let lookat = Vec3::new(2.0, 0.0, 0.1);
     let dist_to_focus = (lookfrom - lookat).mag();
-    let aperture = 2.0;
+    let aperture = dist_to_focus / 125.0;
 
-    let cam = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0,
-                          nx as f32 / ny as f32, aperture, dist_to_focus);
+    let cam = Camera::new(
+        lookfrom,
+        lookat,
+        Vec3::new(0.0, 1.0, 0.0),
+        45.0,
+        nx as f32 / ny as f32,
+        aperture,
+        dist_to_focus,
+    );
 
-    let world: Vec<Box<dyn Hittable>> = vec![
-        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, Rc::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5))))),
-        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))))),
-        Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.3)))),
-        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, Rc::new(Dielectric::new(1.5)))),
-        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.45, Rc::new(Dielectric::new(1.5)))),
-    ];
+    let world = random_scene();
 
     for j in (0..ny).rev() {
         for i in 0..nx {
