1448259a45969709a0ffe2a75f55f20cba57c3de
resttime <resttime@users.noreply.github.com>
2019-12-28 20:54:31 -0600

    Define a MovingSphere struct

--- src/obj.rs
+++ src/obj.rs
@@ -18,3 +18,35 @@ impl Sphere {
         }
     }
 }
+
+pub struct MovingSphere {
+    pub center0: Vec3,
+    pub center1: Vec3,
+    pub time0: f32,
+    pub time1: f32,
+    pub radius: f32,
+    pub material: Rc<dyn Material>,
+}
+
+impl MovingSphere {
+    pub fn new(
+        center0: Vec3,
+        center1: Vec3,
+        t0: f32,
+        t1: f32,
+        radius: f32,
+        material: Rc<dyn Material>,
+    ) -> Self {
+        Self {
+            center0: center0,
+            center1: center1,
+            time0: t0,
+            time1: t1,
+            radius: radius,
+            material: material,
+        }
+    }
+    pub fn center(&self, time: f32) -> Vec3 {
+        self.center0 + ((time - self.time0) / (self.time1 - self.time0)) * (self.center1 - self.center0)
+    }
+}
