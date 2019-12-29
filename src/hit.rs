b721d2070a2b3979a153c3cc0fda91a578cff58f
resttime <resttime@users.noreply.github.com>
2019-12-28 20:54:53 -0600

    Impelment Hittable trait for MovingSphere

--- src/hit.rs
+++ src/hit.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use crate::material::{Material};
-use crate::obj::Sphere;
+use crate::obj::{MovingSphere, Sphere};
 use crate::vec3::{dot, Ray, Vec3};
 
 pub struct HitRecord {
@@ -67,5 +67,35 @@ impl Hittable for Vec<Box<dyn Hittable>> {
             }
         }
         closest_hit
+    }
+}
+
+impl Hittable for MovingSphere {
+    // Solves a quadratic equation
+    fn hit(&self, r: Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let oc = r.origin() - self.center(r.time());
+        let a = dot(r.b, r.b);
+        let b = dot(oc, r.b);
+        let c = dot(oc, oc) - self.radius * self.radius;
+
+        let discriminant = b * b - a * c;
+        if discriminant > 0.0 {
+            // Check smaller parameter
+            let t = (-b - discriminant.sqrt()) / a;
+            if t_min < t && t < t_max {
+                let point = r.point_at_parameter(t);
+                let normal = (point - self.center(r.time())) / self.radius;
+                return Some(HitRecord::new(t, point, normal, Rc::clone(&self.material)));
+            }
+
+            // Check larger parameter
+            let t = (-b + discriminant.sqrt()) / a;
+            if t_min < t && t < t_max {
+                let point = r.point_at_parameter(t);
+                let normal = (point - self.center(r.time())) / self.radius;
+                return Some(HitRecord::new(t, point, normal, Rc::clone(&self.material)));
+            }
+        }
+        None
     }
 }
