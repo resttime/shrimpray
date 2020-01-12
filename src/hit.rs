6b5dc1526befde0d516d98f7b56d9c850aee62f3
resttime <resttime@users.noreply.github.com>
2020-01-11 22:33:07 -0500

    Implement Hittable for XZRect

--- src/hit.rs
+++ src/hit.rs
@@ -230,3 +230,28 @@ impl Hittable for XYRect {
         ))
     }
 }
+
+impl Hittable for XZRect {
+    fn hit(&self, r: Ray, t0: f32, t1: f32) -> Option<HitRecord> {
+        let t = (self.k - r.origin().y()) / r.direction().y();
+        if t < t0 || t > t1 {
+            return None;
+        }
+        let x = r.origin().x() + t * r.direction().x();
+        let z = r.origin().z() + t * r.direction().z();
+        if x < self.x0 || x > self.x1 || z < self.z0 || z > self.z1 {
+            return None;
+        }
+        let u = (x - self.x0) / (self.x1 - self.x0);
+        let v = (z - self.z0) / (self.z1 - self.z0);
+        let p = r.point_at_parameter(t);
+        let normal = Vec3::new(0.0, 0.0, 1.0);
+        Some(HitRecord::new(t, p, normal, u, v, self.material.clone()))
+    }
+    fn bounding_box(&self, _t0: f32, _t1: f32) -> Option<AABB> {
+        Some(AABB::new(
+            Vec3::new(self.x0, self.z0, self.k - 0.0001),
+            Vec3::new(self.x1, self.z1, self.k + 0.0001),
+        ))
+    }
+}
