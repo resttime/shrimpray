6dd59308d22c8b121771981bf81669bf3fba7176
resttime <resttime@users.noreply.github.com>
2020-02-15 18:32:55 -0500

    Add function for giving random unit vector

--- src/util.rs
+++ src/util.rs
@@ -25,6 +25,13 @@ pub fn random_in_unit_disk() -> Vec3 {
     }
 }
 
+pub fn random_unit_vector() -> Vec3 {
+    let a = rand_float() * 2.0 * std::f32::consts::PI;
+    let z = rand_float() * 2.0 - 1.0;
+    let r = (1.0 - z*z).sqrt();
+    return Vec3::new(r*a.cos(), r*a.sin(), z);
+}
+
 pub fn schlick(cosine: f32, ref_idx: f32) -> f32 {
     let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
     r0 = r0 * r0;
