d65094699dd93e8145454513b69dc533845217a3
resttime <resttime@users.noreply.github.com>
2020-02-16 14:45:14 -0500

    Make utility functions inline and add random_cosine_direction

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,13 @@
 use crate::vec3::Vec3;
 use rand::Rng;
 
+#[inline]
 pub fn rand_float() -> f32 {
     let mut rng = rand::thread_rng();
     rng.gen()
 }
 
+#[inline]
 pub fn random_in_unit_sphere() -> Vec3 {
     loop {
         let p =
@@ -16,6 +18,7 @@ pub fn random_in_unit_sphere() -> Vec3 {
     }
 }
 
+#[inline]
 pub fn random_in_unit_disk() -> Vec3 {
     loop {
         let p = 2.0 * Vec3::new(rand_float(), rand_float(), 0.0) - Vec3::new(1.0, 1.0, 0.0);
@@ -25,6 +28,7 @@ pub fn random_in_unit_disk() -> Vec3 {
     }
 }
 
+#[inline]
 pub fn random_unit_vector() -> Vec3 {
     let a = rand_float() * 2.0 * std::f32::consts::PI;
     let z = rand_float() * 2.0 - 1.0;
@@ -32,6 +36,20 @@ pub fn random_unit_vector() -> Vec3 {
     return Vec3::new(r*a.cos(), r*a.sin(), z);
 }
 
+#[inline]
+pub fn random_cosine_direction() -> Vec3 {
+    let r1 = rand_float();
+    let r2 = rand_float();
+    let z = (1.0 - r2).sqrt();
+
+    let phi = 2.0 * std::f32::consts::PI * r1;
+    let x = phi.cos() * r2.sqrt();
+    let y = phi.sin() * r2.sqrt();
+
+    Vec3::new(x, y, z)
+}
+
+#[inline]
 pub fn schlick(cosine: f32, ref_idx: f32) -> f32 {
     let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
     r0 = r0 * r0;
